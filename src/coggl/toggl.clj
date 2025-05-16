4059b3a80ea7e38ffe95d03b9d8c60bd1edf2161
Merveille van Eck <merveillevaneck@gmail.com>
2025-05-17 00:45:19 +0200

    refactored toggl request interface

--- src/coggl/toggl.clj
+++ src/coggl/toggl.clj
@@ -1,56 +1,129 @@
 (ns coggl.toggl
-  (:require [clj-http.client :as http]
-            [buddy.core.codecs.base64 :as b]
-            [buddy.core.codecs :as cod]
-            [jsonista.core :as json]))
-
-(defn str->b64 [token]
-  (->
-   (b/encode token)
-   (cod/bytes->str))
-  )
-   
-
-(defn encode-header [token]
-  (->>
-   (clojure.string/join [token ":api_token"])
-   (str->b64)
-   (str "Basic ")))
-
-(defn decode-body [response]
-  (assoc
-   response
-   :body
-   (json/read-value
-    (:body response)
-    json/keyword-keys-object-mapper)))
-
-(def api {:me "https://api.track.toggl.com/api/v9/me"
-          :workspace "https://api.track.toggl.com/api/v9/workspaces/{workspace_id}"})
-
-(clojure.string/replace (:workspace api) "{workspace_id}" (str 123))
-
-(defn wrap-headers [request token]
-  (-> request
-      (assoc :headers
-             (-> (:headers request)
-                 (assoc "Content-Type" "application/json")
-                 (assoc "Authorization" (encode-header token))))))
+  (:require [coggl.request :as req]
+            [coggl.fs :as files]
+            [coggl.util :as util]
+            [coggl.fs :as fs]))
 
 (defn get-me [token]
-  (->
-   (http/get
-    (:me api)
-    (-> {}
-        (wrap-headers token)))
-   (decode-body)))
+  (-> {:url-key :me :method :get :token token}
+      (req/make-request)
+      (:body)))
+         
+(defn get-default-workspace-id [token]
+  (-> (get-me token)
+      (:default_workspace_id)))
+ 
+(defn get-client-ids-recur
+  ([clients] (get-client-ids-recur clients {}))
+  ([clients m]
+   (if (some? (first clients))
+     (get-client-ids-recur
+      (subvec clients 1)
+      (assoc m
+             (keyword (:name (first clients)))
+             (:id (first clients))))
+     m)))
 
-(defn get-workspace [id token]
-  (->
-   (clojure.string/replace (:workspace api) "{workspace_id}" (str id))
-   (http/get (wrap-headers {} token))
-   (decode-body)))
+(defn get-client-ids [token]
+  (-> (req/make-request
+       {:method :get
+        :url-key :clients
+        :token token
+        :url-params {:workspace_id (get-default-workspace-id token)}})
 
-(defn get-projects [workspace_id token]
+      (:body)
+      (get-client-ids-recur)))
+
+(defn get-default-summary-report [token]
+  (->>
+   (req/make-request {:method :post
+                      :url-key :summary-report
+                      :token token
+                      :url-params {:workspace_id (get-default-workspace-id token)}
+                      :body {
+                             :start_date "2025-01-01"
+                             :end_date "2025-01-31"
+                             :client_ids (vec (vals (get-client-ids token)))
+                      }})
+   (:body)
+   (files/write-report! "test.pdf")
+   ))
+
+(defn get-summary-report-for-client [params]
+  (let [token (:token params)
+        client (:client params)
+        all-client-ids (get-client-ids token)
+        workspace_id (get-default-workspace-id token)]
+    (->>
+     (when (some? (client all-client-ids))
+       (try
+       (req/make-request {:url-key :summary-report
+                          :token token
+                          :method :post
+                          :url-params {:workspace_id workspace_id}
+                          :body {:start_date "2025-01-01"
+                                 :end_date  "2025-01-31"
+                                 :client_ids [(client all-client-ids)]}})
+         (catch Exception _
+           {:body nil}))
+         )
+     (:body)
+     (assoc {} client))))
+
+
+(comment
+  (do
+    (when (not (fs/has-config-dir? "/Users/merv/.test"))
+      (babashka.fs/create-dir "/Users/merv/.test"))
+    (->>
+     (get-summary-report-for-client {:token (util/read-dev-token)
+                                     :client :gamify})
+
+    ;;  (mapv (fn [[k v]] ()))
+    ;;  (:gamify)
+    ;;  (fs/write-report! "/Users/merv/.test/test.pdf")
+     )))
+
+(defn get-summary-report-for-clients [& {:keys [token clients] :as _params}]
+  (let [all-client-ids (get-client-ids token)
+        workspace_id (get-default-workspace-id token)]
+    (->>
+     (mapv (fn [v] (v all-client-ids)) clients)
+     (mapv (fn [client_id]
+             (req/make-request {:url-key :summary-report
+                                :method :post
+                                :url-params {:workspace_id workspace_id}
+                                :body {
+                                       :start_date "2025-02-01"
+                                       :end_date "2025-02-28"
+                                       :client_ids [client_id]
+                                }
+                                :token token})))
+     (mapv (fn [v] (:body v)))
+     (zipmap clients))))
+
+(comment
+  (get-client-ids ())
+  (->>
+  (get-summary-report-for-clients {
+                                   :token (util/read-dev-token)
+                                   :client [:turnstay :simply]})
+   (coggl.fs/write-reports!)
+;;   (first)
+;;   (coggl.fs/write-report "specific.pdf")
+   ))
+
+
+(comment
+  ;; how to make a request
+  (try
+    (req/make-request {:token (util/read-dev-token)
+                       :method :get
+                       :url-key :time-entries
+                       :url-params {}
+                       :body {:start_date "2025-01-01"
+                              :end_date "2025-01-31"
+                              :workspace_id 8731338}})
+    (catch Exception e
+      e))
   )
-
