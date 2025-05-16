3163279acedf9cf77ceaabc850cf5caf2bdc8d5d
Merveille van Eck <merveillevaneck@gmail.com>
2025-05-17 00:42:08 +0200

    added write functions for saving pdf's

--- src/coggl/fs.clj
+++ src/coggl/fs.clj
@@ -1,5 +1,44 @@
 (ns coggl.fs
-  (:require [babashka.fs :as fs]))
+  (:require [babashka.fs :as fs]
+            [buddy.core.codecs :as cod]))
+
+(defn get-config-dir []
+  (str
+   (System/getenv "HOME")
+   "/"
+   ".coggle")) 
+
+(defn has-config-dir? [path]
+  (fs/directory? path))
+
+(defn write-report! [name content]
+  (babashka.fs/write-bytes name (cod/str->bytes content)))
+
+(defn write-reports!
+  ([contents options]
+   (let [extension (:extension options)
+         dir (or
+              (:dir options)
+              (get-config-dir))]
+     (->> (vec (keys contents))
+          (mapv (fn [client]
+                  (when (not (has-config-dir? dir))
+                    (fs/create-dir dir))
+                  (write-report!
+                   (str
+                    (if (some? dir) (str dir "/") "")
+                    (name client)
+                    "."
+                    (name (or extension :pdf)))
+                   (client contents)))))))
+  ([contents]
+   (write-reports! contents {:extension :pdf
+                            :dir (get-config-dir)})))
+
+(comment
+  (write-reports! {:turnstay "some test content"
+                  :simply "some other text"} {:extension "txt" :dir (get-config-dir)})
+  )
 
 (defn has-previous-session? [path]
   (fs/regular-file? path))
