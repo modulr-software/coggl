d8a9f4360fde521b131e8adcd445b57887c6a79b
Merveille van Eck <merveillevaneck@gmail.com>
2025-05-17 16:41:57 +0200

    added util functions to read new config files

--- src/coggl/fs.clj
+++ src/coggl/fs.clj
@@ -1,12 +1,38 @@
 (ns coggl.fs
   (:require [babashka.fs :as fs]
-            [buddy.core.codecs :as cod]))
+            [buddy.core.codecs :as cod]
+            [coggl.config :as conf]
+            [clojure.string :as str]))
+
+;;todo --- we need to create an env validation function using a schema
 
 (defn get-config-dir []
   (str
    (System/getenv "HOME")
    "/"
-   ".coggle")) 
+   ".coggle"))
+
+(defn get-email-template-file []
+  (let [path (:invoice-template-file-path conf/env)]
+    (if (some? path)
+      (java.io.File. path)
+      nil)))
+
+(defn has-password-file? []
+  (let [path (get-in conf/env [:email-password-file-path])]
+    (if (and (some? path)
+             (fs/readable? path))
+      path
+      nil)))
+
+(defn read-gmail-password []
+  (when-let [path (has-password-file?)]
+    (let [value (-> (slurp (:email-password-file-path conf/env))
+                    (str/split #"\n")
+                    (first))]
+      (if (< 0 (count value))
+        value
+        nil))))
 
 (defn has-config-dir? [path]
   (fs/directory? path))
@@ -33,12 +59,11 @@
                    (client contents)))))))
   ([contents]
    (write-reports! contents {:extension :pdf
-                            :dir (get-config-dir)})))
+                             :dir (get-config-dir)})))
 
 (comment
   (write-reports! {:turnstay "some test content"
-                  :simply "some other text"} {:extension "txt" :dir (get-config-dir)})
-  )
+                   :simply "some other text"} {:extension "txt" :dir (get-config-dir)}))
 
 (defn has-previous-session? [path]
   (fs/regular-file? path))
@@ -50,7 +75,7 @@
 
 (defn read-session-file [path]
   (-> path
-   (fs/read-all-lines)
-   (clojure.string/join)))
+      (fs/read-all-lines)
+      (clojure.string/join)))
 
 
